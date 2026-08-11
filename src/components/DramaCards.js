6849003c6c8ca4ae6fc8b51ce72f29fc522fc6c4
Md Forhad Hossen <mdforhadhossen9892@gmail.com>
2026-08-11 10:54:58 +0600

    Update: Made play button smaller on mobile devices

--- src/components/DramaCards.js
+++ src/components/DramaCards.js
@@ -50,7 +50,7 @@ export default function DramaCards({ onWatchClick }) {
                 title="Share this drama"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186j2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185j2.25 2.25 0 00-3.933 2.185z" />
                 </svg>
               </button>
 
@@ -65,9 +65,10 @@ export default function DramaCards({ onWatchClick }) {
                   className="object-cover group-hover:scale-105 transition-transform duration-500"
                 />
                 
-                <div className="absolute inset-0 bg-black/40 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-                  <div className="w-14 h-14 bg-red-600/90 rounded-full flex items-center justify-center shadow-lg transform group-hover:scale-110 transition-transform duration-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 text-white ml-1">
+                {/* মোবাইলে ছোট (w-10 h-10) এবং ডেস্কটপে বড় (md:w-14 md:h-14) প্লে বাটন */}
+                <div className="absolute inset-0 bg-black/30 flex items-center justify-center transition-opacity duration-300 z-10">
+                  <div className="w-10 h-10 md:w-14 md:h-14 bg-red-600/90 rounded-full flex items-center justify-center shadow-lg transform group-hover:scale-110 transition-transform duration-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 md:w-8 md:h-8 text-white ml-0.5">
                       <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
                     </svg>
                   </div>
